7020343143026f21afa3ea865139c537a570ab4b
Brandon <b_lubbehusen@yahoo.com>
2023-04-28 15:03:10 +0200

    locations blade

--- resources/views/locations/index.blade.php
+++ resources/views/locations/index.blade.php
@@ -1,7 +1,8 @@
 <h1>Showing all Locations</h1>
-
+var_export($locations);
+die();
 @forelse ($locations as $location)
-    <li>{{ var_export($location)$location->addressText }}</li>
+    <li>{{ ($location) }}</li>
 @empty
     <p> 'No locations yet' </p>
 @endforelse
