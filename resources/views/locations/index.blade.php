8eb4b3c06bcc48477a4c29adc810290788bd3cfb
Brandon <b_lubbehusen@yahoo.com>
2023-04-28 14:59:56 +0200

    locations blade

--- resources/views/locations/index.blade.php
+++ resources/views/locations/index.blade.php
@@ -1,9 +1,7 @@
 <h1>Showing all Locations</h1>
 
 @forelse ($locations as $location)
-var_export($location);
-die();
-    <li>{{ $location->id }}</li>
+    <li>{{ $location->addressText }}</li>
 @empty
     <p> 'No events yet' </p>
 @endforelse
