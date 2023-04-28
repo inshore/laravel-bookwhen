a416a2af75c7633dd711334a838a0a0b42e4edaf
Brandon <b_lubbehusen@yahoo.com>
2023-04-28 15:23:33 +0200

    locations blade

--- resources/views/locations/index.blade.php
+++ resources/views/locations/index.blade.php
@@ -1,7 +1,7 @@
 <h1>Showing all Locations</h1>
 
 @forelse ($locations as $location)
-<li><a href="./locations/{{ $location->addressText }}">{{ $location->addressText }}</a></li>
+<li>{{ $location->addressText }}</li>
 @empty
     <p> 'No locations yet' </p>
 @endforelse
