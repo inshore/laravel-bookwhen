6f65e6a2e8bab000abc138e7d03b5b3c750104c2
Brandon <b_lubbehusen@yahoo.com>
2023-04-28 15:52:46 +0200

    locations blade

--- src/Http/Controllers/LocationController.php
+++ src/Http/Controllers/LocationController.php
@@ -18,6 +18,8 @@ class LocationController extends Controller
     public function show(string $locationId)
     {
         $location = Bookwhen::location($locationId);
+        var_export($location);
+        die();
         
         return view('bookwhen::locations.show', compact('location'));
     }
