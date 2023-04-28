f039bb0fb17d92b606a11721e737faa8f5ccc0ba
Daniel Mullin <email@danielmullin.com>
2023-04-28 13:02:20 +0200

    updated blades

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -1,7 +1,7 @@
 <h1>Showing all Events</h1>
 
 @forelse ($events as $event)
-    <li>{{ $event->title }}</li>
+    <li><a href="./events/{{ $event->id }}">{{ $event->title }}</a></li>
 @empty
     <p> 'No events yet' </p>
 @endforelse
---
+++ resources/views/events/show.blade.php
@@ -0,0 +1,3 @@
+<h1>{{ $event->title }}</h1>
+
+<p> {{ $event->location->title }}</p>
