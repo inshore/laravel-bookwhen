029dd43a2a38254d7e48d4846115e7d1c939f1df
Brandon <b_lubbehusen@yahoo.com>
2023-04-28 15:59:06 +0200

    locations blade

--- resources/views/attachments/index.blade.php
+++ resources/views/attachments/index.blade.php
@@ -1,7 +1,7 @@
 <h1>Showing all attachments</h1>
 
 @forelse ($attachments as $attachment)
-<li><a href="./attachments/{{ $attachments->title }}">{{ $attachment->title }}</a></li>
+<li><a href="./attachments/{{ $attachment->title }}">{{ $attachment->title }}</a></li>
 @empty
     <p> 'No attachments yet' </p>
 @endforelse
