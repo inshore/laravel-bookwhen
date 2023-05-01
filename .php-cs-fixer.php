24913c44f24ae91a5a2ed1a20d5656929ac72c83
Daniel Mullin <email@danielmullin.com>
2023-05-01 15:57:55 +0200

    fixer

---
+++ .php-cs-fixer.php
@@ -0,0 +1,16 @@
+<?php
+
+//$finder = PhpCsFixer\Finder::create()
+//   ->exclude('somedir')
+//    ->notPath('src/Symfony/Component/Translation/Tests/fixtures/resources.php')
+//    ->in(__DIR__)
+//;
+
+$config = new PhpCsFixer\Config();
+return $config;
+//return $config->setRules([
+//        '@PSR12' => true,
+//        'array_syntax' => ['syntax' => 'short'],
+//    ])
+//    ->setFinder($finder)
+//;
